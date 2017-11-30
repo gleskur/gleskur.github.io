7e7899b24e29e9e96c0ca1ac832bf3eff0040813
gleskur <32885383+gleskur@users.noreply.github.com>
2017-11-30 09:29:46 -0500

    Update main.js

--- selection/js/main.js
+++ selection/js/main.js
@@ -7,19 +7,17 @@ $('header').on('click', function () {
 // 2. When the "Throw a party" button is clicked
 $('#party-time').on('click', function () {
   // a. Remove the boring class from the section with the id boxes
-    $('.boring').removeClass('active');
+    $('#boxes').removeClass('boring');
   // b. Fade out all of the divs in #row4 slowly (hint: your selector could be $('#row4 div') )
     $('#row4 div').fadeOut('slow');
   // c. Add a class snazzy in your css with some styles and add it to all items with the class .box
     $('.box').addClass('snazzy');
-    $('.snazzy').css('color', 'blue');
   // c. Change the styling of the span inside of #box3 by adding a class fun (and style that fun class in CSS)
     $('#box3 span').addClass('fun');
-    $('.fun').css('font-size', '40px');
   // SlideUp all the h2s on the page quickly
-    $('h2').slideUp('quickly');
+    $('h2').slideUp('fast');
   // Add the selected class [already in the CSS] to the divs in #row3
-    $('#row3 div').addClass('this');
+    $('#row3 div').addClass('selected');
   // BONUS: Add a paragraph that says "Time to party!!!" to all boxes with class .add-para (Hint: you'll need to look up the append() method)
   // BONUS: Add an h4 that says "Wild!" to the beginning of #box12 (Hint: you'll need to look up the prepend() method)
 });
@@ -32,23 +30,26 @@ $('#party-time').on('click', function () {
   });
 // 4. When the "Party Pooper" button is clicked
   $('#reset').on('click', function () {
+  // a. Slide down the header
     $('header').slideDown('slow');
+  // b. Fade out the footer
     $('footer').fadeOut('slow');
+  // c. Show the divs in #row4
     $('#row4 div').show();
+  // d. Add the boring class to the element with the id boxes
     $('#boxes').addClass('boring');
-    $('.snazzy').removeClass('active');
-    $('#box3 span').removeClass('active');
-    $('this #row3 div').removeClass('active');
+  // e. Remove the snazzy class from everything
+    $('.box').removeClass('snazzy');
+  // f. Remove the fun class from the span in #box3
+    $('#box3 span').removeClass('fun');
+    // g. Remove the selected class from the divs in #row3
+    $('#row3 div').removeClass('selected');
+    // h. Slide down all of the h2s
     $('h2').slideDown('slow');
   });
-  // a. Slide down the header
-  // b. Fade out the footer
-  // c. Show the divs in #row4
-  // d. Add the boring class to the element with the id boxes
-  // e. Remove the snazzy class from everything
-  // f. Remove the fun class from the span in #box3
-  // g. Remove the selected class from the divs in #row3
-  // h. Slide down all of the h2s
+
+
   // BONUS: If you completed the above bonus tasks, look up "remove jquery"
   // and then see if you can remove the paragraphs and h3s that you added in.
   // make sure to ONLY remove those paragraphs and h3s you added, not all the paragraphs and h3s!
+
